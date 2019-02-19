ca00a8fae8fe8b87290d565e902be4cbc3dac592
Andrew DiBella <andrew.dibella1@marist.edu>
2019-02-19 16:26:59 -0500

    created the hello world page using express

---
+++ server/oregonTrail.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 1337
+
+app.get('/', (req,res)=> res.send('Hello World!'))
+app.listen(port,()=> console.log(`server running on port ${port}`))
