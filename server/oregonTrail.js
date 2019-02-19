129fa5c4e445f127527fb1f16cc0fe548775afcc
Andrew DiBella <andrew.dibella1@marist.edu>
2019-02-19 18:26:57 -0500

    reached an issue

--- server/oregonTrail.js
+++ server/oregonTrail.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+app.use(express.static('client/public'));
 const port = 1337
 
-app.get('/', (req,res)=> res.send('Hello World!'))
+
+app.get('/', function (req, res) {
+res.sendFile('index.html', {root: './client/views' })
+})
+
 app.listen(port,()=> console.log(`server running on port ${port}`))
