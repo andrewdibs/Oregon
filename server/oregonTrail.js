8c7f59558cb9f8b69b83ca8af123d023170a798b
Andrew DiBella <andrew.dibella1@marist.edu>
2019-03-13 14:29:12 -0400

    server file finished

--- server/oregonTrail.js
+++ server/oregonTrail.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 app.use(express.static('client/public'));
-const port = 1337
+const port = 1337;
 
 
 app.get('/', function (req, res) {
 res.sendFile('index.html', {root: './client/views' })
-})
+});
 
-app.listen(port,()=> console.log(`server running on port ${port}`))
+app.get('/mainmenu', function (req, res) {
+res.sendFile('mainmenu.html', {root: './client/views' })
+});
+
+app.get('/topten', function (req, res) {
+res.sendFile('topten.html', {root: './client/views' })
+});
+
+app.get('/setup', function (req, res) {
+res.sendFile('setup.html', {root: './client/views' })
+});
+
+app.get('/trail', function (req, res) {
+res.sendFile('trail.html', {root: './client/views' })
+});
+
+app.listen(port,()=> console.log(`server running on port ${port}`));
